746d7e8d2f4ba8088f0ea4caadbc5ea750df08ee
Pragati Negi <pragatinegi7@gmail.com>
2026-02-12 01:08:27 +0530

    Create Permutation.cpp

---
+++ Permutation /Permutation.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void permute(string str, int l, int r) {
+    if(l == r) {
+        cout << str << endl;
+        return;
+    }
+
+    for(int i = l; i <= r; i++) {
+        swap(str[l], str[i]);        
+        permute(str, l+1, r);        
+        swap(str[l], str[i]);       
+    }
+}
+
+int main() {
+    string str;
+
+    cout << "Enter string: ";
+    cin >> str;
+
+    permute(str, 0, str.length()-1);
+
+    return 0;
+}
+
