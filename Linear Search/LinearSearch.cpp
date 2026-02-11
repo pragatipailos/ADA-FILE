af2e186977846d6017f0aff47565c2de371ac04c
Pragati Negi <pragatinegi7@gmail.com>
2026-02-12 01:05:00 +0530

    Create LinearSearch.cpp

---
+++ Linear Search/LinearSearch.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, key;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter elements:\n";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Enter element to search: ";
+    cin >> key;
+
+    int found = 0;
+
+    for(int i = 0; i < n; i++) {
+        if(arr[i] == key) {
+            cout << "Element found at position " << i+1;
+            found = 1;
+            break;
+        }
+    }
+
+    if(found == 0)
+        cout << "Element not found";
+
+    return 0;
+}
