9fd54ba17ab273ae64f093822126fcbb7b860c19
Pragati Negi <pragatinegi7@gmail.com>
2026-02-12 01:15:08 +0530

    Add Graph.py to visualize time complexity

---
+++ Duplicate No/Graph.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+n = np.linspace(1, 100, 100)
+time = n**2
+
+plt.plot(n, time)
+plt.title("Time Complexity of Finding Duplicates O(n^2)")
+plt.xlabel("Input Size (n)")
+plt.ylabel("Time")
+plt.show()
